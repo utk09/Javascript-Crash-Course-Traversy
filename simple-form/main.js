3d0ad7783fe180eec4507f3b8855afc0ae7fd345
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-07-31 15:30:39 +0530

    dom manipulation

--- simple-form/main.js
+++ simple-form/main.js
@@ -19,4 +19,8 @@
 
 const ul = document.querySelector('.items');
 
-ul.remove();
+// // ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello World!';
+ul.children[1].innerText = 'Zumba';
+ul.lastElementChild.innerHTML = '<h2>Hello</h2>';
