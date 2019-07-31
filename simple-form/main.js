99b8ea9df1ff96bcce56eb6098a6dd7c2a6a7e51
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-07-31 23:19:54 +0530

    added dom

--- simple-form/main.js
+++ simple-form/main.js
@@ -62,3 +62,24 @@
 //     document.querySelector('body').classList.add('bg-dark');
 // })
 
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const usrList = document.querySelector("#users");
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput.value === '') {
+        console.log('Failed!')
+        msg.classList.add('error');
+        msg.innerHTML = 'Please fill all fields';
+
+        
+    } else {
+        console.log("Success!")
+    }
+}
