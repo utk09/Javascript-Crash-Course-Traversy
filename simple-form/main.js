e84f4eda247efa5bde206e8d951e2b68068a4f3a
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-07-31 15:21:14 +0530

    looping over items

--- simple-form/main.js
+++ simple-form/main.js
@@ -9,5 +9,10 @@ console.log(document.querySelector('h1'));
 
 // Multiple Element Selctor
 
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('item'));
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => console.log(item));
