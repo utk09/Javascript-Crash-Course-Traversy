97a2507a0dee54a83c2483545150a03413fbef8d
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-07-31 15:17:40 +0530

    added selectors

--- simple-form/main.js
+++ simple-form/main.js
@@ -2,4 +2,12 @@
 
 // console.log(window);  // uncomment this and check in console.
 
-//Single Element Slector
+// Single Element Slector
+
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+// Multiple Element Selctor
+
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByClassName('item'));
