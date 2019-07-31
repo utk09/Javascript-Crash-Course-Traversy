65fef8ad4402d4422dc934a5e6aa1a0a7479a6d1
utkarsh <utkarsh.tiwari@somaiya.edu>
2019-07-31 22:57:50 +0530

    newFile.js

--- simple-form/newFile.js
+++ simple-form/newFile.js
@@ -1,5 +1,5 @@
 // // ul.remove();
 // ul.lastElementChild.remove();
 // ul.firstElementChild.textContent = 'Hello World!';
-ul.children[1].innerText = 'Zumba';
-ul.lastElementChild.innerHTML = '<h2>Hello</h2>';
+// ul.children[1].innerText = 'Zumba';
+// ul.lastElementChild.innerHTML = '<h2>Hello</h2>';
